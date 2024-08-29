90e361d33dc43090b6785bf9a9a24175deba7b61
seulgi <tmfrl1590@gmail.com>
2024-08-29 22:12:00 +0900

    #3 공통 Color, Dimens 값 설정

---
+++ presentation/src/main/java/com/party/presentation/ui/theme/Dimens.kt
@@ -0,0 +1,18 @@
+package com.party.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val EXTRA_LARGE_BUTTON_HEIGHT = 56.dp
+val LARGE_BUTTON_HEIGHT = 48.dp
+val MEDIUM_BUTTON_HEIGHT = 36.dp
+val SMALL_BUTTON_HEIGHT = 24.dp
+
+val LARGE_FONT_SIZE = 16.sp
+val MEDIUM_FONT_SIZE = 14.sp
+val SMALL_FONT_SIZE = 12.sp
+
+val LARGE_CORNER_SIZE = 16.dp
+val MEDIUM_CORNER_SIZE = 12.dp
+
+val MEDIUM_PADDING_SIZE = 20.dp
