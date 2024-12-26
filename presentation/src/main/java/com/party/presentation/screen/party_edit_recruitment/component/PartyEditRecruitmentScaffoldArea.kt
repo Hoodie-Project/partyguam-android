e040e56ca37fb06a158a271faba7a9219ca3a6dc
seulgi <tmfrl1590@gmail.com>
2024-12-27 00:06:36 +0900

    refactor #205 모집수정 페이지 타이틀 간격 수정

--- presentation/src/main/java/com/party/presentation/screen/party_edit_recruitment/component/PartyEditRecruitmentScaffoldArea.kt
+++ presentation/src/main/java/com/party/presentation/screen/party_edit_recruitment/component/PartyEditRecruitmentScaffoldArea.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.party.common.R
+import com.party.common.WidthSpacer
+import com.party.common.component.icon.DrawableIcon
 import com.party.common.component.icon.DrawableIconButton
 import com.party.common.component.scaffold.ScaffoldCenterBar
+import com.party.common.noRippleClickable
 import com.party.common.ui.theme.BLACK
 import com.party.common.ui.theme.T2
 
@@ -39,13 +43,14 @@ fun PartyEditRecruitmentScaffoldArea(
                     fontWeight = FontWeight.Bold,
                     fontSize = T2
                 )
-                DrawableIconButton(
+                WidthSpacer(widthDp = 4.dp)
+                DrawableIcon(
+                    modifier = Modifier
+                        .noRippleClickable { onShowHelpCard(true) },
                     icon = painterResource(id = R.drawable.icon_help),
                     contentDescription = "help",
-                    onClick = { onShowHelpCard(true) }
                 )
             }
-
         },
         actionIcons = {
             DrawableIconButton(
