ec401f51c617b7cd66fb36dff34df881b923ef89
seulgi <tmfrl1590@gmail.com>
2025-02-26 12:40:27 +0900

    Feat: Develop "Close Recruitment" button UI for recruitment edit screen

--- common/src/main/java/com/party/common/component/RecruitmentListItem4.kt
+++ common/src/main/java/com/party/common/component/RecruitmentListItem4.kt
@@ -1,6 +1,8 @@
 package com.party.common.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -8,19 +10,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.party.common.HeightSpacer
+import com.party.common.R
 import com.party.common.TextComponent
 import com.party.common.WidthSpacer
+import com.party.common.component.icon.DrawableIconButton
 import com.party.common.convertIsoToCustomDateFormat
 import com.party.common.ui.theme.B2
 import com.party.common.ui.theme.B3
@@ -43,61 +54,94 @@ fun RecruitmentListItem4(
     applicationCount: Int,
     onClick: () -> Unit,
 ) {
-    Card(
-        onClick = onClick,
+    var isShowFinishButton by remember { mutableStateOf(false) }
+
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(130.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = WHITE,
-        ),
-        shape = RoundedCornerShape(LARGE_CORNER_SIZE),
-        border = BorderStroke(1.dp, GRAY100),
-        elevation = CardDefaults.cardElevation(4.dp),
-    ) {
-        Column(
+    ){
+        Card(
+            onClick = onClick,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 20.dp, vertical = 16.dp)
+                .fillMaxWidth()
+                .height(130.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = WHITE,
+            ),
+            shape = RoundedCornerShape(LARGE_CORNER_SIZE),
+            border = BorderStroke(1.dp, GRAY100),
+            elevation = CardDefaults.cardElevation(4.dp),
         ) {
-            TextComponent(
-                text = "모집일 ${convertIsoToCustomDateFormat(createdAt)}",
-                fontSize = B3,
-                textColor = GRAY500
-            )
-            HeightSpacer(heightDp = 8.dp)
-            TextComponent(
-                text = "$main | $sub",
-                fontSize = T3,
-                fontWeight = FontWeight.SemiBold,
-            )
-            HeightSpacer(heightDp = 20.dp)
-
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(20.dp),
-                verticalAlignment = Alignment.CenterVertically,
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp, vertical = 16.dp)
             ) {
-                if(status == "completed"){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     TextComponent(
-                        text = "모집마감",
-                        fontSize = B2,
+                        text = "모집일 ${convertIsoToCustomDateFormat(createdAt)}",
+                        fontSize = B3,
+                        textColor = GRAY500
                     )
-                }else{
-                    RecruitingCountArea(
-                        text = "모집중",
-                        number = "$recruitedCount / $recruitingCount",
-                        textColor = RED
-                    )
-                    WidthSpacer(widthDp = 24.dp)
-                    RecruitingCountArea(
-                        text = "지원자",
-                        number = "$applicationCount",
-                        textColor = PRIMARY
+
+                    DrawableIconButton(
+                        icon = painterResource(id = R.drawable.icon_vertical_more2),
+                        contentDescription = "",
+                        onClick = {
+                            isShowFinishButton = true
+                        },
+                        modifier = Modifier
+                            .size(24.dp)
                     )
                 }
+
+                HeightSpacer(heightDp = 8.dp)
+                TextComponent(
+                    text = "$main | $sub",
+                    fontSize = T3,
+                    fontWeight = FontWeight.SemiBold,
+                )
+                HeightSpacer(heightDp = 20.dp)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    if(status == "completed"){
+                        TextComponent(
+                            text = "모집마감",
+                            fontSize = B2,
+                        )
+                    }else{
+                        RecruitingCountArea(
+                            text = "모집중",
+                            number = "$recruitedCount / $recruitingCount",
+                            textColor = RED
+                        )
+                        WidthSpacer(widthDp = 24.dp)
+                        RecruitingCountArea(
+                            text = "지원자",
+                            number = "$applicationCount",
+                            textColor = PRIMARY
+                        )
+                    }
+                }
             }
+        }
+
+        if(isShowFinishButton){
+            RecruitmentFinishButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+            )
         }
     }
 }
@@ -125,6 +169,39 @@ private fun RecruitingCountArea(
             textColor = textColor,
             modifier = Modifier.alignByBaseline() // Baseline에 맞춤
         )
+    }
+}
+
+@Composable
+fun RecruitmentFinishButton(
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .width(143.dp)
+            .height(62.dp)
+            .padding(end = 12.dp, bottom = 12.dp),
+        shape = RoundedCornerShape(LARGE_CORNER_SIZE),
+        colors = CardDefaults.cardColors(
+            containerColor = WHITE
+        ),
+        border = BorderStroke(1.dp, GRAY100),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.CenterStart
+        ){
+            TextComponent(
+                text = "마감하기",
+                fontSize = B2,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .width(103.dp)
+                    .height(22.dp)
+                    .padding(horizontal = 20.dp)
+            )
+        }
     }
 }
 
@@ -156,4 +233,10 @@ private fun RecruitmentListItem4Preview1() {
         status = "completed",
         onClick = {},
     )
+}
+
+@Preview
+@Composable
+private fun RecruitmentFinishButtonPreview() {
+    RecruitmentFinishButton()
 }
